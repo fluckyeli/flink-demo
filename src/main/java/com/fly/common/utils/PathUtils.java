7cddc8e4ca6766a0567a09963928f21d8905a7ef
fluck <fluckyeli@163.com>
2026-04-07 23:29:41 +0800

    删除没有实现的cdc代码，新增flink包目录，学习flink demo

--- src/main/java/com/fly/common/utils/PathUtils.java
+++ src/main/java/com/fly/common/utils/PathUtils.java
@@ -1,7 +1,6 @@
-package com.fly.flinkcdc.utils;
+package com.fly.common.utils;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URISyntaxException;
