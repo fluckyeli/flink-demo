2dba5ae04410411d780e960f304c1b8e1ab8fa7c
fluckyeli <fluckyeli@163.com>
2026-04-08 17:23:41 +0800

    设置本地webui和并行度测试WordCountParallelismDemo

---
+++ src/main/java/com/fly/flink/WordCountParallelismDemo.java
@@ -0,0 +1,42 @@
+package com.fly.flink;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class WordCountParallelismDemo {
+    public static void main(String[] args) throws Exception {
+        // 1. 创建本地环境，webui访问： http://localhost:8081
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(new Configuration());
+        env.setParallelism(2); // 全局指定并行度
+
+        // 2. 从socket流中读取数据
+        DataStreamSource<String> source = env.socketTextStream("localhost", 9999);
+
+        source.flatMap((FlatMapFunction<String, Tuple2<String, Integer>>) (in, out) -> {
+                    String[] words = in.split("\\s+");
+                    for (String word : words) {
+                        // 3.2 转换
+                        Tuple2<String, Integer> wordCountOne = Tuple2.of(word, 1);
+                        out.collect(wordCountOne);
+                    }
+                })
+                .returns(Types.TUPLE(Types.STRING, Types.INT))
+                .setParallelism(3) // attention 指定算子并行度，优先级高于全局指定的并行度
+                .keyBy(tuple -> tuple.f0)// 3.3
+                .sum(1)// 3.4 聚合
+                .print();
+
+        // 5. 执行
+        env.execute();
+
+        /*
+        attention 算子优先级: 算子 > 代码env 全局设定 > 提交jar包指定参数 > flink-conf.yaml配置
+         */
+
+        env.close();
+    }
+}
