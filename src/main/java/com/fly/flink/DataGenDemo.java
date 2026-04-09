a2fc7fc0732ce600720c66ed7f26e8386ed622a6
fluckyeli <fluckyeli@163.com>
2026-04-09 21:18:45 +0800

    DataGenDemo 测试使用数据生成器

---
+++ src/main/java/com/fly/flink/DataGenDemo.java
@@ -0,0 +1,32 @@
+package com.fly.flink;
+
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.connector.source.util.ratelimit.RateLimiterStrategy;
+import org.apache.flink.connector.datagen.source.DataGeneratorSource;
+import org.apache.flink.connector.datagen.source.GeneratorFunction;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class DataGenDemo {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+
+        DataGeneratorSource<String> stringDataGeneratorSource = new DataGeneratorSource<>(new GeneratorFunction<Long, String>() {
+            @Override
+            public String map(Long aLong) {
+                return "s_" + aLong;
+            }
+        }
+                , 10
+                , RateLimiterStrategy.perSecond(1)
+                , Types.STRING
+        );
+
+        env.fromSource(stringDataGeneratorSource, WatermarkStrategy.noWatermarks(), "data-gen-source").print();
+
+        env.execute();
+        env.close();
+    }
+}
